711ad3b3386125c99360fe3ec517f4a511f5c3ac
David H. Brown <david_h_brown@uri.edu>
2018-06-14 01:11:45 -0400

    AnalogInput class compiles
    untested; way past bedtime.

---
+++ LaserScarecrow/AnalogInput.cpp
@@ -0,0 +1,68 @@
+/*
+   License GPL-2.0
+   Part of the URI Laser Scarecrow project
+   https://github.com/davidhbrown-uri/laser-scarecrow-arduino
+*/
+
+#include "AnalogInput.h"
+AnalogInput::AnalogInput(byte pin)
+{
+  AnalogInput(pin, ANALOGINPUT_DEFAULT_READINGS);
+}
+AnalogInput::AnalogInput(byte pin, byte numReadings)
+{
+  AnalogInput(pin, numReadings, ANALOGINPUT_DEFAULT_MS);
+}
+AnalogInput::AnalogInput(byte pin, byte numReadings, unsigned int ms)
+{
+  AnalogInput(pin, numReadings, ms, ANALOGINPUT_DEFAULT_CHANGE_THRESHOLD);
+}
+AnalogInput::AnalogInput(byte pin, byte numReadings, unsigned int ms, int changeThreshold)
+{
+  _pin = pin;
+  _numReadings = constrain(numReadings, 0, ANALOGINPUT_READING_ARRAY_SIZE);
+  _changeThreshold = max(0, changeThreshold);
+  _msBetweenReads = max(0, ms);
+  pinMode(_pin, INPUT);
+  _value = analogRead(_pin);
+  _lastReadMillis = millis();
+  for (_readIndex = 0; _readIndex < _numReadings; _readIndex++)
+  {
+    _readings[_readIndex] = _value;
+  }
+  _readIndex = 0;
+  _hasNewValue = false;
+}
+void AnalogInput::process()
+{
+  if (_lastReadMillis + _msBetweenReads < millis())
+  {
+    _lastReadMillis = millis();
+    _readings[_readIndex++] = analogRead(_pin);
+    _readIndex %= _numReadings;
+    unsigned long sum = 0L;
+    for (byte i = 0; i < _numReadings; i++)
+    {
+      sum += (unsigned long) _readings[i];
+    }
+    int average = (int)(sum / _numReadings);
+    if (abs(average - _value) > _changeThreshold)
+    {
+      _value = average;
+      _hasNewValue = true;
+    }// if change
+  } // if time for a reading
+}
+bool AnalogInput::hasNewValue()
+{
+  return _hasNewValue;
+}
+int AnalogInput::getValue()
+{
+  return _value;
+}
+void AnalogInput::acknowledgeNewValue()
+{
+  _hasNewValue = false;
+}
+
---
+++ LaserScarecrow/AnalogInput.h
@@ -0,0 +1,40 @@
+/*
+   License GPL-2.0
+   Part of the URI Laser Scarecrow project
+   https://github.com/davidhbrown-uri/laser-scarecrow-arduino
+ */
+
+#ifndef AnalogInput_h
+#define AnalogInput_h
+#include "Arduino.h"
+
+#define ANALOGINPUT_DEFAULT_READINGS 3
+#define ANALOGINPUT_DEFAULT_MS 100
+#define ANALOGINPUT_DEFAULT_CHANGE_THRESHOLD 51
+//5% of full range 
+#define ANALOGINPUT_READING_ARRAY_SIZE 8
+class AnalogInput
+{
+  public:
+    AnalogInput(byte pin);
+    AnalogInput(byte pin, byte numReadings);
+    AnalogInput(byte pin, byte numReadings, unsigned int ms);
+    AnalogInput(byte pin, byte numReadings, unsigned int ms, int threshold);
+    void process(); // possibly read the pin if enough time has elapsed 
+    bool hasNewValue();
+    void acknowledgeNewValue();
+    int getValue();
+
+  private:
+    byte _pin;
+    byte _numReadings;
+    int _readings[ANALOGINPUT_READING_ARRAY_SIZE];
+    int _changeThreshold;
+    unsigned int _msBetweenReads;
+    unsigned long _lastReadMillis;
+    bool _hasNewValue;
+    int _value;
+    byte _readIndex;
+};
+
+#endif
